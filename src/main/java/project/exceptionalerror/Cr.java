20b8be667998fe5721e99c2678d58dd4adf31495
Sabbir <sabbir772002@gmail.comgit config --global user.name sabbir772002>
2022-10-18 23:25:58 +0600

    Initial commit

---
+++ src/main/java/project/exceptionalerror/Cr.java
@@ -0,0 +1,19 @@
+package project.exceptionalerror;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @Paromita Chowdhury @Sabbir Hossain @Nusrat Hossain @Asfia Haide rEla
+ */
+public class Cr {
+    String id,day, time;
+    public Cr(String id,String day, String time){
+        this.id=id;
+        this.day=day;
+        this.time=time;
+    }
+}
---
+++ src/main/java/project/exceptionalerror/ExceptionalError.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package project.exceptionalerror;
+
+/**
+ *
+ * @Paromita Chowdhury @Sabbir Hossain @Nusrat Hossain @Asfia Haide rEla
+ *
+ */
+public class ExceptionalError {
+
+    public static void main(String[] args) {
+       new Mainframe().setVisible(true);
+    }
+}
